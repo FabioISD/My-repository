4cc935766257ee33caae2f7e6ff24a2dd1d0b270
Fábio Henrique Medeiros Bezer <fabio.bezerra@pc-8.isd.matriz>
2019-08-27 08:02:20 -0300

    Atividades

---
+++ AtividadeProgramação/IMC.py
@@ -0,0 +1,5 @@
+nome = str( input('Qual é seu nome? '))
+peso = float (input ('Qual é seu peso(kg)? '))
+altura = float (input ('Qual sua altura(m) '))
+imc = (peso/(altura*altura))
+print ('Seu IMC?:' ,imc)
